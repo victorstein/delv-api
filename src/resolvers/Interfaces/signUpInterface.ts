8145d685f2a8fe73366867683ce243d5f4dcbe40
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-29 19:43:52 -0600

    Removed sign up interface in favor of user related interfaces. for individual exports

--- src/resolvers/Interfaces/signUpInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ArgsType } from 'type-graphql'
-import { IsPhoneNumber } from 'class-validator'
-
-@ArgsType()
-export default class signUpInterface {
-  @Field({ nullable: false })
-  @IsPhoneNumber('NI', { message: 'Por favor ingrese un numero de telefono valido.' })
-  phoneNumber!: string
-}
