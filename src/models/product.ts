3b2d33dbd22c5179b36694fa2f9dc46d4bc44a6b
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-30 20:06:52 -0600

    created the product model

---
+++ src/models/product.ts
@@ -0,0 +1,32 @@
+import { ObjectType, Field, ID } from "type-graphql";
+import { Typegoose, prop } from "@hasezoey/typegoose";
+
+@ObjectType()
+export class Product extends Typegoose {
+  @Field(() => ID)
+  id: string
+
+  @prop({ required: true })
+  @Field({ nullable: false, description: 'This field contains the id of the business it belongs to' })
+  owner: string
+
+  @prop({ required: true })
+  @Field({ nullable: false })
+  name: string
+
+  @prop()
+  @Field()
+  picture: string
+
+  @prop({ required: true })
+  @Field({ nullable: false })
+  price: number
+
+  @prop()
+  @Field()
+  description: string
+
+  @prop({ default: true })
+  @Field()
+  available: true
+}
